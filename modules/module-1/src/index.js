378cf8658af004bc0c9d41275f942517b1de38b3
nivaldojsneto <nivaldojsneto@gmail.com>
2019-02-08 19:26:25 -0300

    update application

--- modules/module-1/src/index.js
+++ modules/module-1/src/index.js
@@ -1,9 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 
-class App extends Component {
+class Button extends Component {
   render() {
-    return <h1>Hello Nivaldo Neto</h1>;
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+Button.defaultProps = {
+  children: 'Salvar',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string,
+};
+
+class App extends Component {
+  handlerClick() {
+    alert('Botão clicado');
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Nivaldo Neto</h1>
+        <Button
+          onClick={() => {
+            alert('Botão 1');
+          }}
+        />
+        <Button onClick={this.handlerClick}>EnviarNovo</Button>
+      </Fragment>
+    );
   }
 }
 
