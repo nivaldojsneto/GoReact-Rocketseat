de0da9853b3e8b1c23521ee4486ef914cc19eb8c
nivaldojsneto <nivaldojsneto@gmail.com>
2019-02-08 21:46:32 -0300

    update application

--- challenges/01/src/components/App.js
+++ challenges/01/src/components/App.js
@@ -8,19 +8,43 @@ export default class App extends Component {
     posts: [
       {
         id: 1,
-        name: 'Diego Fernandes',
-        avatar: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4',
+        name: 'Pedro Renan',
+        avatar: 'https://avatars3.githubusercontent.com/u/923983?s=400&v=4',
         time: 'há 3 min',
         body:
-          'Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis',
+          'Para improvisar é preciso estar preparado',
       },
       {
         id: 2,
-        name: 'Rocketseat',
-        avatar: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4',
-        time: 'há 30 min',
+        name: 'Gabriel Buzzi',
+        avatar: 'https://avatars1.githubusercontent.com/u/9993834?s=400&v=4',
+        time: 'há 5 min',
         body:
-          'Pellentesque, lacinia dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel posuere posuere, rutrum eu ipsum.',
+          'Faaaaalaaaaaa Galeeeeraaaa Beleeeezaaaaaaaaaaa?',
+      },
+      {
+        id: 3,
+        name: 'Allysson Silva',
+        avatar: 'https://avatars3.githubusercontent.com/u/41745648?s=400&v=4',
+        time: 'há 10 min',
+        body:
+          'Eaeeee ta baum?',
+      },
+      {
+        id: 4,
+        name: 'Thiago',
+        avatar: 'https://avatars0.githubusercontent.com/u/37192076?s=400&v=4',
+        time: 'há 15 min',
+        body:
+          'Tá na Disney é?',
+      },
+      {
+        id: 2,
+        name: '@cryptor',
+        avatar: 'https://avatars2.githubusercontent.com/u/19333594?s=400&v=4',
+        time: 'há 20 min',
+        body:
+          'Bora hackear tudo pessoal!!',
       },
     ],
   };
--- challenges/01/src/components/Header.js
+++ challenges/01/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <div className="header">
-    <h3>Nivaldo Neto - Front End | PRTE </h3>
+    <h3>Nivaldo Neto - Front End | PRTE - Começando no React</h3>
   </div>
 );
 
